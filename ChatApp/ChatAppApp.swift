632d11e8b45ab68e73e9d0633d262d702b3dd076
Cihat Tasci <cihattasci97@gmail.com>
2023-03-10 11:49:00 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Cihat Tascı on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
